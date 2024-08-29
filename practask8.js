6a2e77d1516b4392ae5cc2ca439eaafd5fc0579e
golamsarwar98 <golamsarwar1896@gmail.com>
2024-08-30 01:23:02 +0600

    ternary practice

---
+++ practask8.js
@@ -0,0 +1,14 @@
+let num1 = 15;
+let num2 = 30;
+
+//Ternary operator
+let result = num1>num2? num1*2 : num1+num2;
+
+console.log(result);
+
+//General if-else
+if(num1>num2){
+    console.log(num1*2);
+}
+else
+    console.log(num1+num2);
