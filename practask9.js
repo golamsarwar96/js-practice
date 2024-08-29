e3eb22d3f8ed0ac1bd3817d973e7fab0ac92397b
golamsarwar98 <golamsarwar1896@gmail.com>
2024-08-30 01:31:22 +0600

    ticket fare calculate

---
+++ practask9.js
@@ -0,0 +1,16 @@
+let age = 60;
+let isStudent = false;
+
+if(age < 10){
+    console.log("free");
+}
+else if(isStudent){
+    console.log("50% discount");
+}
+else if(age >=60){
+    console.log("15% discount");
+}
+else{
+    console.log("Ticket fare 800 tk");
+}
+
