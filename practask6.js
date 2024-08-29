8b6988e5f35fbd9b5264ce6dd13b19c08f87ee31
golamsarwar98 <golamsarwar1896@gmail.com>
2024-08-30 01:09:43 +0600

    grading system program

---
+++ practask6.js
@@ -0,0 +1,12 @@
+let examScore = 94;
+
+if(examScore>=90 && examScore<=100)
+    console.log("A");
+else if(examScore>=80 && examScore<=89)
+    console.log("B");
+else if(examScore>=70 && examScore<=79)
+    console.log("C");
+else if(examScore>=60 && examScore<=69)
+    console.log("D");
+else
+    console.log("F");
